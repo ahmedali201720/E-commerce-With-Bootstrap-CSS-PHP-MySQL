acac4b79ee402c2dd61e25b777d396c1afbaa2ea
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2020-10-03 13:22:03 +0200

    slider optimized (with fixed behaviour)

--- resources/templates/front/slider.php
+++ resources/templates/front/slider.php
@@ -5,13 +5,8 @@
         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
-
             <?php get_active_slide(); ?>
-
             <?php get_slides(); ?>
-
-
-
     </div>
     <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
