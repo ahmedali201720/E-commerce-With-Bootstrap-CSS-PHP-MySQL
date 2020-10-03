396fc829d40f6a56d4bbf6fd0fb0916748956036
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2020-10-03 13:46:56 +0200

    adding redirection after deleting

--- resources/templates/back/delete_report.php
+++ resources/templates/back/delete_report.php
@@ -3,6 +3,7 @@ if(isset($_GET['id'])) {
 $query = query("DELETE FROM reports WHERE report_id = " . escape_string($_GET['id']) . " ");
 confirm($query);
 set_message("Report Deleted");
+  redirect("../../../public/admin/index.php?reports");
 } else {
 redirect("../../../public/admin/index.php?reports");
 }
