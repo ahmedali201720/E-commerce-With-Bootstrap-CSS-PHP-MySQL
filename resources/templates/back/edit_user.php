627f9c9d3523f9b1dc74aa8bc5ee043df939918b
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2020-10-03 13:39:51 +0200

    Update edit_user.php

--- resources/templates/back/edit_user.php
+++ resources/templates/back/edit_user.php
@@ -19,31 +19,22 @@
                         <div class="col-md-6">
 
                            <div class="form-group">
-                           
                             <input type="file" name="file">
-                               
                            </div>
-
-
                            <div class="form-group">
                             <label for="username">Username</label>
                             <input type="text" name="username" class="form-control"  >
-                               
                            </div>
-
-
                             <div class="form-group">
                                 <label for="first name">First Name</label>
                             <input type="text" name="first_name" class="form-control"  >
-                               
                            </div>
 
                             <div class="form-group">
                                 <label for="last name">Last Name</label>
                             <input type="text" name="last_name" class="form-control" >
                                
-                           </div>
-
+                          </div>
 
                             <div class="form-group">
                                 <label for="password">Password</label>
@@ -57,11 +48,7 @@
 
                             <input type="submit" name="update_user" class="btn btn-primary pull-right" value="Update" >
                                
-                           </div>
-
-
-                            
-
+                           </div>      
                         </div>
 
                       
