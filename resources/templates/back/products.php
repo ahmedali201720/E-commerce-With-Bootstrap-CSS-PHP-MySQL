987f80c83fefbe9ad8374e59c0e5327f4b4e5ae6
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2020-10-03 13:32:35 +0200

    products functionality

--- resources/templates/back/products.php
+++ resources/templates/back/products.php
@@ -6,10 +6,7 @@
 
 <h3 class="bg-success"><?php display_message(); ?></h3>
 <table class="table table-hover">
-
-
     <thead>
-
       <tr>
            <th>Id</th>
            <th>Title</th>
@@ -19,13 +16,6 @@
       </tr>
     </thead>
     <tbody>
-
-      
       <?php get_products_in_admin(); ?>
-
-
   </tbody>
 </table>
-
-
-
