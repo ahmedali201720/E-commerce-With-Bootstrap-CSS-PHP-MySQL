1f52db68ca0c0bf9d0769eb4dec4094fbdbbc4c0
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2020-10-03 13:33:17 +0200

    orders functionality (final)

--- resources/templates/back/orders.php
+++ resources/templates/back/orders.php
@@ -3,7 +3,6 @@
 <div class="row">
 <h1 class="page-header">
    All Orders
-
 </h1>
 
 <h4 class= "bg-success"><?php display_message(); ?></h4>
@@ -12,14 +11,12 @@
 <div class="row">
 <table class="table table-hover">
     <thead>
-
       <tr>
            <th>id</th>
            <th>Amount</th>
            <th>Transaction</th>
            <th>Currency</th>
            <th>Sattus</th>
-   
       </tr>
     </thead>
     <tbody>
