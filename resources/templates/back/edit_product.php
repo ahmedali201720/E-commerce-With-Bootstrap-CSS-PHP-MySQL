b23feff203767dedb9f0bd6d05f1ae4256612c7c
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2020-10-03 13:45:04 +0200

    edit product final

--- resources/templates/back/edit_product.php
+++ resources/templates/back/edit_product.php
@@ -1,15 +1,9 @@
 
 <?php 
-
-
 if(isset($_GET['id'])) {
-
-
 $query = query("SELECT * FROM products WHERE product_id = " . escape_string($_GET['id']) . " ");
 confirm($query);
-
 while($row = fetch_array($query)) {
-
 $product_title          = escape_string($row['product_title']);
 $product_category_id    = escape_string($row['product_category_id']);
 $product_price          = escape_string($row['product_price']);
@@ -17,53 +11,25 @@ $product_description    = escape_string($row['product_description']);
 $short_desc             = escape_string($row['short_desc']);
 $product_quantity       = escape_string($row['product_quantity']);
 $product_image          = escape_string($row['product_image']);
-
-
-
 $product_image = display_image($row['product_image']);
-
-
-
-    }
-
-
-update_product();
-
-
-
 }
-
-
-
-
-
- ?>
-
-
-
+update_product();
+}
+?>
 
 <div class="col-md-12">
-
-<div class="row">
-<h1 class="page-header">
-   Edit Product
-</h1>
-</div>
-               
-
+  <div class="row">
+     <h1 class="page-header">
+        Edit Product
+     </h1>
+    </div>
 
 <form action="" method="post" enctype="multipart/form-data">
-
-
 <div class="col-md-8">
-
 <div class="form-group">
     <label for="product-title">Product Title </label>
         <input type="text" name="product_title" class="form-control" value="<?php echo $product_title; ?>">
-       
     </div>
-
-
     <div class="form-group">
            <label for="product-title">Product Description</label>
       <textarea name="product_description" id="" cols="30" rows="10" class="form-control"><?php echo $product_description; ?></textarea>
@@ -85,11 +51,7 @@ update_product();
            <label for="product-title">Product Short Description</label>
       <textarea name="short_desc" id="" cols="30" rows="3" class="form-control"><?php echo $short_desc; ?></textarea>
     </div>
-
-
-
-
-</div><!--Main Content-->
+</div>
 
 
 <!-- SIDEBAR-->
@@ -117,12 +79,7 @@ update_product();
             <?php show_categories_add_product_page(); ?>
            
         </select>
-
-
 </div>
-
-
-
 
 
     <!-- Product Brands-->
@@ -136,13 +93,6 @@ update_product();
 
 <!-- Product Tags -->
 
-
-   <!--  <div class="form-group">
-          <label for="product-title">Product Keywords</label>
-          <hr>
-        <input type="text" name="product_tags" class="form-control">
-    </div>
- -->
     <!-- Product Image -->
     <div class="form-group">
         <label for="product-title">Product Image</label>
@@ -152,10 +102,5 @@ update_product();
       
     </div>
 
-
-
 </aside><!--SIDEBAR-->
-
-
-    
 </form>
